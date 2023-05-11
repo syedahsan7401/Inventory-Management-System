200a63091173516e2f8705f9f18c55104a3186c1
syedahsan <73951797+syedahsan7401@users.noreply.github.com>
2023-05-11 13:24:14 +0500

    hello
    smak

---
+++ Inventory Management SYstem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_Management_SYstem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
